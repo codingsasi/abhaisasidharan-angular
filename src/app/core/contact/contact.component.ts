74a2e065ef095ae781414cf280640394062b533d
abhaisasidharan <abhaisasidharan@gmail.com>
2019-02-02 05:57:51 +0530

    Added contact and sidebar

--- src/app/core/contact/contact.component.ts
+++ src/app/core/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
-  popup = 'page';
+  @Input() view: string = 'page';
+
   constructor() { }
 
   ngOnInit() {
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,13 +4,14 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { AppRoutingModule } from '../app-routing.module';
 import { ContactComponent } from './contact/contact.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   imports: [
     CommonModule,
     AppRoutingModule,
   ],
-  declarations: [HeaderComponent, FooterComponent, ContactComponent],
-  exports: [HeaderComponent, FooterComponent]
+  declarations: [HeaderComponent, FooterComponent, ContactComponent, SidebarComponent],
+  exports: [HeaderComponent, FooterComponent, ContactComponent, SidebarComponent]
 })
 export class CoreModule { }
